f3c4360450b6da6e94bb5eda1faba8f118a69307
mosecoza <mosecoza@gmail.com>
2024-03-15 13:34:43 +0200

    feat: add sticky page header

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="flex justify-center items-center z-50 top-0 left-0 w-full py-4 sticky bg-slate-950">
+      <h1 className="text-orange-300">Surviving Mars: One Organized Task at a Time</h1>
+    </header>
+  )
+}
+
+export default Header
