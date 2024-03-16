d83bd84e6d9e094df1860e5453df13c4700c8d7d
mosecoza <mosecoza@gmail.com>
2024-03-16 23:31:45 +0200

    feat: edit task fuctionality

--- app/components/TaskCard.tsx
+++ app/components/TaskCard.tsx
@@ -1,37 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Task } from "@/app/interfaces/Task";
 import useDisclosure from "@/app/hooks/useDisclosure";
 import { useTasksStore } from "@/app/store/useTasks";
 import { useUsersStore } from "@/app/store/users";
 import AlertModal from "@/app/components/modals/AlertModal";
+import { CardButton } from "./CardButton";
+import AddTaskModal from "./modals/AddTaskModal";
 
 interface Props {
   task: Task;
 }
 
 const TaskCard = ({ task }: Props) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    isOpen: isOpenDeleteAlert,
+    onOpen: onOpenDeleteAlert,
+    onClose: onCloseDeleteAlert,
+  } = useDisclosure();
+
+  const {
+    isOpen: isOpenUpdateModal,
+    onOpen: onOpenUpdateModal,
+    onClose: onCloseUpdateModal,
+  } = useDisclosure();
+  const {
+    isOpen: isOpenViewModal,
+    onOpen: onOpenViewModal,
+    onClose: onCloseViewModal,
+  } = useDisclosure();
+
   const { deleteTask } = useTasksStore();
   const { getUserById } = useUsersStore();
 
-  const handleDeleteTask = () =>{
+  const handleDeleteTask = () => {
     deleteTask(task.id);
-    onClose()
-  }
+    onCloseDeleteAlert();
+  };
 
   return (
     <div
       key={task.id}
       className=" my-4 group rounded-lg border  px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 dark:border-gray-300 hover:dark:bg-neutral-800/30"
     >
-      {isOpen && (
+      {isOpenDeleteAlert && (
         <AlertModal
-          isOpen={isOpen}
+          isOpen={isOpenDeleteAlert}
           message={`Are you sure you want to delete this task?`}
-          onClose={onClose}
+          onClose={onCloseDeleteAlert}
           handleContinue={handleDeleteTask}
         />
       )}
+      {isOpenUpdateModal && <AddTaskModal currentTask={task} open={isOpenUpdateModal} onClose={onCloseUpdateModal} />}
       <div className="flex items-center justify-between lg:flex-row sm:flex-col xs:flex-col mb-4 ">
         <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white capitalize">
           {task.title}
@@ -49,18 +68,13 @@ const TaskCard = ({ task }: Props) => {
           By: <b>{getUserById(task.createdBy)?.name ?? "User not found"}</b>
         </p>
         <div className="flex items-center justify-between">
-          <button
-            onClick={onOpen}
-            className={`m-0 max-w-[30ch] text-sm opacity-50 mr-2 hover:underline text-red-600`}
-          >
-            Delete
-          </button>
-          <a
-            href="#"
-            className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
-          >
-            View
-          </a>
+          <CardButton variant="primary" title="View" handleClick={onOpenViewModal} />
+          <CardButton variant="warning" title="Update" handleClick={onOpenUpdateModal} />
+          <CardButton
+            variant="danger"
+            title="Delete"
+            handleClick={onOpenDeleteAlert}
+          />
         </div>
       </div>
     </div>
--- app/components/modals/AddTaskModal.tsx
+++ app/components/modals/AddTaskModal.tsx
@@ -2,29 +2,26 @@ import { Task } from "@/app/interfaces/Task";
 import { useTasksStore } from "@/app/store/useTasks";
 import { useUsersStore } from "@/app/store/users";
 import { generateTaskId } from "@/app/utils/generateTaskId";
-import React, {
-  ChangeEvent,
-  Dispatch,
-  FormEvent,
-  SetStateAction,
-  useState,
-} from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 
 interface Props {
   open: boolean;
-  onClose: Dispatch<SetStateAction<boolean>>;
+  onClose: () => void;
+  currentTask?: Task;
 }
 
-const AddTaskModal = ({ open = false, onClose }: Props) => {
+const AddTaskModal = ({ open = false, currentTask, onClose }: Props) => {
   const { users, currentUser } = useUsersStore();
-  const { tasks,addTask } = useTasksStore()
-  const [task, setTask] = useState<Task>({
-    id: generateTaskId(),
-    createdBy: currentUser?.id ?? 0,
-    description: "",
-    assignedTo: 0,
-    title: "",
-  });
+  const { addTask, updateTask } = useTasksStore();
+  const [task, setTask] = useState<Task>(
+    currentTask ?? {
+      id: generateTaskId(),
+      createdBy: currentUser?.id ?? 0,
+      description: "",
+      assignedTo: 0,
+      title: "",
+    }
+  );
 
   const handleChange = (
     e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement>
@@ -38,10 +35,10 @@ const AddTaskModal = ({ open = false, onClose }: Props) => {
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if(!task.createdBy) throw "User not found please reload and try again";
-    addTask({...task, id: generateTaskId()})
-    
-  }
+    if (!task.createdBy) throw "User not found please reload and try again";
+    currentTask ? updateTask(task) : addTask({ ...task, id: generateTaskId() });
+    onClose();
+  };
 
   return (
     <div
@@ -58,13 +55,13 @@ const AddTaskModal = ({ open = false, onClose }: Props) => {
           {/* Modal header */}
           <div className="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white sen">
-              Add New Task
+              {`${currentTask ? "Update Task" : "Add New Task"}`}
             </h3>
             <button
               type="button"
               className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
               data-modal-toggle="defaultModal"
-              onClick={() => onClose(false)}
+              onClick={onClose}
             >
               <svg
                 aria-hidden="true"
@@ -146,7 +143,7 @@ const AddTaskModal = ({ open = false, onClose }: Props) => {
             </div>
             <button
               type="submit"
-              className="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              className="text-blue-500 dark:text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
             >
               <svg
                 className="mr-1 -ml-1 w-6 h-6"
